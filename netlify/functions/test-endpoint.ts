be36c5dbdf42c7134145cca2f9feb65a1042824c
profdl <112704467+profdl@users.noreply.github.com>
2024-11-04 17:46:38 -0500

    more troubleshooting

--- netlify/functions/test-endpoint.ts
+++ netlify/functions/test-endpoint.ts
@@ -1,6 +1,6 @@
-import { Handler } from '@netlify/functions';
+const { Handler } = require('@netlify/functions');
 
-export const handler: Handler = async (event) => {
+exports.handler = async (event) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
