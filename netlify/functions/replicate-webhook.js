f67f91b9e1ef841626b15ba8203d3bbc8920adbb
profdl <112704467+profdl@users.noreply.github.com>
2024-11-04 20:51:41 -0500

    another

--- netlify/functions/replicate-webhook.js
+++ netlify/functions/replicate-webhook.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabase = createClient(
+let supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
