2b23050f901c23cbada7ebaa738f00c2b53768d3
profdl <112704467+profdl@users.noreply.github.com>
2024-11-20 07:21:12 -0500

    WORKING. Except for pose, which I am fixing now.

--- src/store.ts
+++ src/store.ts
@@ -503,10 +503,11 @@ export const useStore = create<BoardState>((set, get) => ({
         }
 
         if (controlShape.showPose && controlShape.posePreviewUrl) {
-          workflow["17"].inputs.conditioning = [currentConditioningNode, 0];
+          workflow["18"].inputs.conditioning = [currentConditioningNode, 0];
           workflow["19"].inputs.image = controlShape.posePreviewUrl;
-          currentConditioningNode = "17";
+          currentConditioningNode = "18";
         }
+
       }
 
       workflow["3"].inputs.positive = [currentConditioningNode, 0];
